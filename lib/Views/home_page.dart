e1aff888acd1290ad07b796c92199a841b7f8408
lalo1883 <eduardonunez975@gmail.com>
2024-10-24 15:51:21 -0600

    views on process

--- lib/Views/home_page.dart
+++ lib/Views/home_page.dart
@@ -12,6 +12,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
@@ -24,20 +26,33 @@ class _HomePageState extends State<HomePage> {
               width: double.infinity,
               color: Colors.black,
               child: Padding(
-                padding: const EdgeInsets.only(top:80.0),
+                padding: const EdgeInsets.only(top: 70),
                 child: Text("LAPERFL", textAlign: TextAlign.center, style: GoogleFonts.lora(color: Colors.white, fontSize: 20),),
               ),
             ),
           ),
+          // SizedBox(height: screenHeight * .05,),
           Expanded(
-            flex: 3,
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 18.0),
+              child: Container(
+                // color: Colors.grey,
+                child: Text("   Just type the name of the\nperfume you want to  review", style: GoogleFonts.lora(fontSize: 15),),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 4,
             child: Container(
+              // color: Colors.green,
               child: Text("data"),
             ),
           ),
           Expanded(
-            flex: 2,
+            flex: 1,
             child: Container(
+              // color: Colors.green,
               child: Text("data"),
             ),
           ),
